5d559d6abf704b744d954cb390a59064dadc3074
Martin Redington <martin.redington@hudl.com>
2015-06-17 16:04:28 +0100

    First attempt at style fixes

--- MPCTest/MultipeerController.swift
+++ MPCTest/MultipeerController.swift
@@ -14,8 +14,7 @@ class MultipeerController: NSObject, MCSessionDelegate, MCNearbyServiceAdvertise
     var session: MCSession
     var nearbyAdvertiser: MCNearbyServiceAdvertiser
     
-    init(displayName: String)
-    {
+    init(displayName: String) {
         let peerID = MCPeerID(displayName: displayName);
         session = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .None)
         nearbyAdvertiser = MCNearbyServiceAdvertiser(peer: peerID, discoveryInfo: nil, serviceType: "mpc-demo")
@@ -23,18 +22,15 @@ class MultipeerController: NSObject, MCSessionDelegate, MCNearbyServiceAdvertise
         nearbyAdvertiser.delegate = self
     }
     
-    func beginAdvertising()
-    {
+    func beginAdvertising() {
         self.nearbyAdvertiser.startAdvertisingPeer()
     }
     
-    func endAdvertising()
-    {
+    func endAdvertising() {
         self.nearbyAdvertiser.stopAdvertisingPeer()
     }
     
-    func distributeImage(image: UIImage, timestamp: Float64)
-    {
+    func distributeImage(image: UIImage, timestamp: Float64) {
         let imageData = UIImageJPEGRepresentation(image, 0.4)
         let packet = [ "image" : imageData, "timestamp" : timestamp ]
         
@@ -43,10 +39,8 @@ class MultipeerController: NSObject, MCSessionDelegate, MCNearbyServiceAdvertise
         self.session.sendData(packetData, toPeers: self.session.connectedPeers, withMode: .Unreliable, error: nil)
     }
     
-    func advertiser(advertiser: MCNearbyServiceAdvertiser!, didReceiveInvitationFromPeer peerID: MCPeerID!, withContext context: NSData!, invitationHandler: ((Bool, MCSession!) -> Void)!)
-    {
-        if((invitationHandler) != nil)
-        {
+    func advertiser(advertiser: MCNearbyServiceAdvertiser!, didReceiveInvitationFromPeer peerID: MCPeerID!, withContext context: NSData!, invitationHandler: ((Bool, MCSession!) -> Void)!) {
+        if invitationHandler != nil {
             invitationHandler(true, self.session)
         }
     }
@@ -56,38 +50,31 @@ class MultipeerController: NSObject, MCSessionDelegate, MCNearbyServiceAdvertise
         {
         case .Connected:
             NSLog("peer connected = %@", peerID.displayName)
-            break
         case .NotConnected:
             NSLog("peer not connected = %@", peerID.displayName)
-            break
         case .Connecting:
             NSLog("peer connected = %@", peerID.displayName)
-            break
         }
     }
     
     
     // Received data from remote peer
-    func session(session: MCSession!, didReceiveData data: NSData!, fromPeer peerID: MCPeerID!)
-    {
+    func session(session: MCSession!, didReceiveData data: NSData!, fromPeer peerID: MCPeerID!) {
         
     }
     
     // Received a byte stream from remote peer
-    func session(session: MCSession!, didReceiveStream stream: NSInputStream!, withName streamName: String!, fromPeer peerID: MCPeerID!)
-    {
-        
+    func session(session: MCSession!, didReceiveStream stream: NSInputStream!, withName streamName: String!, fromPeer peerID: MCPeerID!) {
+
     }
     
     // Start receiving a resource from remote peer
-    func session(session: MCSession!, didStartReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!)
-    {
+    func session(session: MCSession!, didStartReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!) {
         
     }
     
     // Finished receiving a resource from remote peer and saved the content in a temporary location - the app is responsible for moving the file to a permanent location within its sandbox
-    func session(session: MCSession!, didFinishReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, atURL localURL: NSURL!, withError error: NSError!)
-    {
+    func session(session: MCSession!, didFinishReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, atURL localURL: NSURL!, withError error: NSError!) {
         
     }
    
--- MPCTest/ReceiverViewController.swift
+++ MPCTest/ReceiverViewController.swift
@@ -54,10 +54,8 @@ class ReceiverViewController: UIViewController, MCSessionDelegate {
     // Received data from remote peer
     func session(session: MCSession!, didReceiveData data: NSData!, fromPeer peerID: MCPeerID!)
     {
-        if let dictionary = NSKeyedUnarchiver.unarchiveObjectWithData(data) as? NSDictionary
-        {
-            if let pictureData: NSData = dictionary["image"] as? NSData
-            {
+        if let dictionary = NSKeyedUnarchiver.unarchiveObjectWithData(data) as? NSDictionary {
+            if let pictureData = dictionary["image"] as? NSData {
                 let image = UIImage(data: pictureData)
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
                     self.imageView?.image = image
@@ -67,20 +65,17 @@ class ReceiverViewController: UIViewController, MCSessionDelegate {
     }
     
     // Received a byte stream from remote peer
-    func session(session: MCSession!, didReceiveStream stream: NSInputStream!, withName streamName: String!, fromPeer peerID: MCPeerID!)
-    {
+    func session(session: MCSession!, didReceiveStream stream: NSInputStream!, withName streamName: String!, fromPeer peerID: MCPeerID!) {
         
     }
     
     // Start receiving a resource from remote peer
-    func session(session: MCSession!, didStartReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!)
-    {
+    func session(session: MCSession!, didStartReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!) {
         
     }
     
     // Finished receiving a resource from remote peer and saved the content in a temporary location - the app is responsible for moving the file to a permanent location within its sandbox
-    func session(session: MCSession!, didFinishReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, atURL localURL: NSURL!, withError error: NSError!)
-    {
+    func session(session: MCSession!, didFinishReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, atURL localURL: NSURL!, withError error: NSError!) {
         
     }
 
